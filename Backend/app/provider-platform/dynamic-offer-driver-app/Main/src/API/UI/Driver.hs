071405ba7cb2f902f460134113c1de9f26adc68b
ErasedSoul <anirbandas0206@gmail.com>
2023-11-17 22:41:12 +0530

    compile fix

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/API/UI/Driver.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/API/UI/Driver.hs
@@ -54,6 +54,7 @@ import Kernel.External.Maps (LatLong)
 import Kernel.Types.APISuccess (APISuccess)
 import Kernel.Types.Id
 import Kernel.Utils.Common
+import Lib.RoutesTh ()
 import Servant
 import Tools.Auth
 
